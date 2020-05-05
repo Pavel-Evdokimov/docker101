e56adf50275e45b41032d547fe095e3199edfe6f
Pavel Evdokimov <p_evdokimov@chebit24.ru>
2020-05-05 20:01:23 +0300

    generate files added

--- public/src/main.js
+++ public/src/main.js
@@ -1,1 +1,16 @@
 console.log("This is test");
+document.getElementById("the-button").addEventListener("click", (e) => {
+    fetch("/api/item", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(new Date().toJSON())
+    }).then((response) => {
+        return response.json();
+    }).then((value) => {
+        console.log(value)
+    }).catch((err) => {
+        console.error(err);
+    });
+});
--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
+const path = require('path');
 const PORT = process.env.PORT || 3000;
-
 app.use(express.static('public'));
+
+app.post('/api/item', (req, res) => {
+    fs.appendFile(path.join("data", "demo.txt"), new Date().toJSON(), (err) => {
+        if (err) {
+            console.error(err);
+            res.json({err: true, message: err.message});
+        }
+        console.log("success");
+        res.json({err: false, message: "success"});
+    });
+});
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
